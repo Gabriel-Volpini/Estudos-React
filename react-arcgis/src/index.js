9954eb301ef18dfb0cb5e350dbe385b8430f8917
Gabriel <bielpini@yahoo.com.br>
2020-02-14 22:49:33 -0300

    Implementacao de marker personalizado

--- react-arcgis/src/index.js
+++ react-arcgis/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { loadCss } from 'esri-loader';
-import { Scene } from '@esri/react-arcgis';
+import { Map, Scene } from '@esri/react-arcgis';
 import { useState, useEffect } from 'react';
 import { loadModules } from 'esri-loader';
+import mark from './marker.svg'
  
 const BermudaTriangle = (props) => {
  
@@ -14,19 +15,16 @@ const BermudaTriangle = (props) => {
          // First create a point geometry (this is the location of the Titanic)
         var point = {
             type: "point", // autocasts as new Point()
-            latitude: -19.9813463,
-            longitude: -43.9460538
+            latitude: props.lat,
+            longitude: props.lng
           };
   
           // Create a symbol for drawing the point
           var markerSymbol = {
-            type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-            color: [226, 119, 40],
-            outline: {
-              // autocasts as new SimpleLineSymbol()
-              color: [255, 255, 255],
-              width: 2
-            }
+            type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
+  url: "https://media-exp1.licdn.com/dms/image/C4D0BAQGNptgoajwi9g/company-logo_200_200/0?e=1590019200&v=beta&t=C8jyq0TGYFkl3JOW9ap5FCBEIJASJV5kFo5AHiEs4TM",
+  width: "64px",
+  height: "64px"
           };
   
           // Create a graphic and add the geometry and symbol to it
@@ -51,7 +49,14 @@ const BermudaTriangle = (props) => {
 ReactDOM.render(
     <div style={{ width: '100vw', height: '100vh' }}>
         <Scene class="full-screen-map">
-            <BermudaTriangle />
+            <BermudaTriangle 
+                lat={-19.9813463}
+                lng={-43.9460538}
+            />
+            <BermudaTriangle 
+                lat={-43.9460538}
+                lng={-19.9813463}
+            />
         </Scene>
     </div>,
   document.getElementById('root')

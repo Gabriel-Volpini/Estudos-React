27a5979161e06874eab4a4dfdff89fb9e623e902
Gabriel <bielpini@yahoo.com.br>
2020-02-14 23:16:17 -0300

    Constry destry polyline

--- react-arcgis/src/index.js
+++ react-arcgis/src/index.js
@@ -10,54 +10,73 @@ const BermudaTriangle = (props) => {
  
     const [graphic, setGraphic] = useState(null);
     useEffect(() => {
- 
+        if(!props.limpa)return
         loadModules(['esri/Graphic']).then(([Graphic]) => {
          // First create a point geometry (this is the location of the Titanic)
-        var point = {
-            type: "point", // autocasts as new Point()
-            latitude: props.lat,
-            longitude: props.lng
+         var polyline = {
+            type: "polyline",  // autocasts as new Polyline()
+              paths: [
+                [-111.30, 52.68],
+                [-98, 49.5],
+                [-93.94, 29.89]
+              ]
           };
-  
-          // Create a symbol for drawing the point
-          var markerSymbol = {
-            type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
-  url: "https://media-exp1.licdn.com/dms/image/C4D0BAQGNptgoajwi9g/company-logo_200_200/0?e=1590019200&v=beta&t=C8jyq0TGYFkl3JOW9ap5FCBEIJASJV5kFo5AHiEs4TM",
-  width: "64px",
-  height: "64px"
+          
+          var polylineSymbol = {
+            type: "simple-line",  // autocasts as SimpleLineSymbol()
+            color: [226, 119, 40],
+            width: 4
           };
-  
-          // Create a graphic and add the geometry and symbol to it
-          var pointGraphic = new Graphic({
-            geometry: point,
-            symbol: markerSymbol
-          }); 
+          
+          var polylineAtt = {
+            Name: "Keystone Pipeline",
+            Owner: "TransCanada"
+          };
+          
+          var polylineGraphic = new Graphic({
+            geometry: polyline,
+            symbol: polylineSymbol,
+            attributes: polylineAtt
+          });
+          
 
-            setGraphic(pointGraphic);
-            props.view.graphics.add(pointGraphic);
+            setGraphic(polylineGraphic);
+            props.view.graphics.add(polylineGraphic);
         }).catch((err) => console.error(err));
  
-        return function cleanup() {
+        return function cleanup() { //UnMount
             props.view.graphics.remove(graphic);
         };
-    }, []);
+    }, [props.limpa ]);
+
+    useEffect(() => {
+        if(props.limpa)return
+
+        props.view.graphics.remove(graphic);
+    },[props.limpa])
  
     return null;
  
 }
- 
-ReactDOM.render(
-    <div style={{ width: '100vw', height: '100vh' }}>
+ function Teste({children}){
+    
+    const [graphic, setGraphic] = useState(false);
+    
+
+     return<div style={{ width: '100vw', height: '100vh' }}><button onClick={()=>setGraphic(!graphic)}>aaa</button>
         <Scene class="full-screen-map">
-            <BermudaTriangle 
-                lat={-19.9813463}
-                lng={-43.9460538}
-            />
-            <BermudaTriangle 
-                lat={-43.9460538}
-                lng={-19.9813463}
-            />
-        </Scene>
+
+                <BermudaTriangle 
+                    limpa={graphic}
+                    lat={-43.9460538}
+                    lng={-19.9813463}
+                />
+            </Scene>
+     </div>
+ }
+ReactDOM.render(
+    <div >
+        <Teste/>
     </div>,
   document.getElementById('root')
 );
